2aae0d880b7307314d0fc3ba93aed8a95186c9d3
palsss15 <palsss15>
2021-12-30 12:00:35 +0530

    heyaaa!!

---
+++ List1/List1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace List1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> mylist = test(new List<int>(new int[] { 1, 2, 3, 4 }));
+            foreach (var i in mylist)
+            {
+                Console.Write(i.ToString() + " ");
+            }
+            Console.ReadLine();
+        }
+        public static List<int> test(List<int> nums)
+        {
+            IEnumerable<int> e = nums.Select(x => 5 * (x + 2));
+            return e.ToList();
+            //Console.ReadLine();
+        }
+
+        //Console.ReadLine();
+    }
+ 
+}
